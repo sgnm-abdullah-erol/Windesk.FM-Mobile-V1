b2e8309533679f052f4a2f3bd4406b8882933c41
muratcanyusufoglu <muratcan.ysfgl@gmail.com>
2023-07-12 16:55:45 +0300

    TopBar Line Number

--- lib/product/screens/home/screens/work_order_list/view/work_order_list_screen.dart
+++ lib/product/screens/home/screens/work_order_list/view/work_order_list_screen.dart
@@ -1,6 +1,7 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:vm_fm_4/feature/components/loading/custom_main_loading.dart';
 
 import '../../../../../../feature/components/appbar/custom_main_appbar.dart';
 import '../../../../../../feature/components/loading/custom_appbar_loading.dart';
@@ -21,11 +22,7 @@ class WorkOrderListScreen extends StatelessWidget {
       create: (context) => WorkOrderListProvider(),
       child: Consumer(
         builder: (context, WorkOrderListProvider provider, _) {
-          return provider.isLoading
-              ? const CustomAppbarLoading(
-                  appBar: CustomMainAppbar(title: Text(AppStrings.workOrderList), returnBack: true, elevation: 3),
-                )
-              : _BuildScaffold(provider: provider);
+          return _BuildScaffold(provider: provider);
         },
       ),
     );
@@ -38,9 +35,9 @@ class _BuildScaffold extends StatelessWidget {
   final WorkOrderListProvider provider;
 
   final List<Widget> tabs = const [
-    Text(AppStrings.myOrders),
-    Text(AppStrings.myGroups),
-    Text(AppStrings.myPendiks),
+    Text(AppStrings.myOrders, maxLines: 1, overflow: TextOverflow.ellipsis),
+    Text(AppStrings.myGroups, maxLines: 1, overflow: TextOverflow.ellipsis),
+    Text(AppStrings.myPendiks, maxLines: 1, overflow: TextOverflow.ellipsis),
   ];
 
   @override
@@ -52,11 +49,14 @@ class _BuildScaffold extends StatelessWidget {
           children: [
             const SizedBox(height: 10),
             CustomDefaultTabController(provider: provider, tabs: tabs),
-            provider.tabIndex == 0
-                ? MyWorkOrders(provider: provider)
-                : provider.tabIndex == 1
-                    ? MyGroupWorkOrders(provider: provider)
-                    : MyWorkOrderPendiks(provider: provider),
+            provider.isLoading
+                ? const CustomMainLoading(
+                  )
+                : provider.tabIndex == 0
+                    ? MyWorkOrders(provider: provider)
+                    : provider.tabIndex == 1
+                        ? MyGroupWorkOrders(provider: provider)
+                        : MyWorkOrderPendiks(provider: provider),
           ],
         ),
       ),
