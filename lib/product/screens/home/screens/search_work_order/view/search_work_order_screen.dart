f11597f9c1768c83014ceb858a106135813bf651
MertSaygili <mert71719601@gmail.com>
2023-07-18 10:44:09 +0300

    search work order screen base added

---
+++ lib/product/screens/home/screens/search_work_order/view/search_work_order_screen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SearchWorkOrderScreen extends StatelessWidget {
+  const SearchWorkOrderScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
