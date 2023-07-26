adf01dae21899bc90a6067bb5c6c1c2da5c81163
MertSaygili <mert71719601@gmail.com>
2023-07-26 13:17:02 +0300

    loading changed

--- lib/feature/components/loading/custom_main_loading.dart
+++ lib/feature/components/loading/custom_main_loading.dart
@@ -1,12 +1,47 @@
+import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
+
+import '../../constants/other/colors.dart';
 
 class CustomMainLoading extends StatelessWidget {
   const CustomMainLoading({super.key});
 
+  final String _windesk = 'Windesk.';
+  final String _fm = 'FM';
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(child: CircularProgressIndicator()),
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Align(alignment: Alignment.center, child: CircularProgressIndicator(color: APPColors.Main.blue)),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                _windesk,
+                style: Theme.of(context).textTheme.titleMedium?.copyWith(
+                      color: APPColors.Main.blue,
+                    ),
+              ),
+              const SizedBox(width: 10),
+              AnimatedTextKit(
+                animatedTexts: [
+                  RotateAnimatedText(
+                    _fm,
+                    textStyle: Theme.of(context).textTheme.titleMedium?.copyWith(
+                          color: APPColors.Login.blue,
+                        ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ],
+      ),
     );
   }
 }
--- lib/feature/service/global_services.dart/auth_service/auth_service_repository_impl.dart
+++ lib/feature/service/global_services.dart/auth_service/auth_service_repository_impl.dart
@@ -9,8 +9,7 @@ import 'auth_service_repository.dart';
 
 class AuthServiceRepositoryImpl extends AuthServiceRepository {
   @override
-  Future<Either<LoginModel, CustomServiceException>> login(
-      String username, String password) async {
+  Future<Either<LoginModel, CustomServiceException>> login(String username, String password) async {
     @override
     String url = 'http://10.0.2.2:3012/user/login';
 
@@ -28,15 +27,13 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
       return Left(loginModel);
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.loginError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.loginError, statusCode: '400'));
     }
   }
 
   @override
-  Future<Either<bool, CustomServiceException>> logout(
-      String refreshToken, String token) async {
-    String url = 'http://localhost:3012/user/logout';
+  Future<Either<bool, CustomServiceException>> logout(String refreshToken, String token) async {
+    String url = 'http://10.0.2.2:3012/user/logout';
 
     try {
       final response = await super.dio.post(
@@ -54,24 +51,20 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
         if (data[ServiceResponseStatusEnums.success.rawText] == true) {
           return const Left(true);
         } else {
-          return Right(CustomServiceException(
-              message: CustomServiceMessages.logoutError, statusCode: '400'));
+          return Right(CustomServiceException(message: CustomServiceMessages.logoutError, statusCode: '400'));
         }
       } else {
-        return Right(CustomServiceException(
-            message: CustomServiceMessages.logoutError, statusCode: '400'));
+        return Right(CustomServiceException(message: CustomServiceMessages.logoutError, statusCode: '400'));
       }
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.logoutError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.logoutError, statusCode: '400'));
     }
   }
 
   @override
-  Future<Either<CheckAccesTokenModel, CustomServiceException>> checkAccessToken(
-      String token) async {
-    String url = 'http://localhost:3012/user/checkAccessToken';
+  Future<Either<CheckAccesTokenModel, CustomServiceException>> checkAccessToken(String token) async {
+    String url = 'http://10.0.2.2:3012/user/checkAccessToken';
 
     try {
       final response = await super.dio.post(url,
@@ -84,19 +77,15 @@ class AuthServiceRepositoryImpl extends AuthServiceRepository {
       if (response.statusCode == 200 || response.statusCode == 201) {
         final data = response.data;
 
-        CheckAccesTokenModel checkAccesTokenModel =
-            CheckAccesTokenModel.fromJson(data);
+        CheckAccesTokenModel checkAccesTokenModel = CheckAccesTokenModel.fromJson(data);
 
         return Left(checkAccesTokenModel);
       } else {
-        return Right(CustomServiceException(
-            message: CustomServiceMessages.tokenChangeError,
-            statusCode: '400'));
+        return Right(CustomServiceException(message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
       }
     } catch (error) {
       super.logger.e(error.toString());
-      return Right(CustomServiceException(
-          message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
+      return Right(CustomServiceException(message: CustomServiceMessages.tokenChangeError, statusCode: '400'));
     }
   }
 }
