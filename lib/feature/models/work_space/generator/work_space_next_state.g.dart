e7da1375e3133a00598bd7aa904a86b31532a1a8
MertSaygili <mert71719601@gmail.com>
2023-11-09 08:20:43 +0300

    unneccesary codes deleted

--- lib/feature/models/work_space/generator/work_space_next_state.g.dart
+++ lib/feature/models/work_space/generator/work_space_next_state.g.dart
@@ -12,9 +12,7 @@ NextState _$NextStateFromJson(Map<String, dynamic> json) => NextState(
       className: json['className'] as String?,
       isActive: json['isActive'] as bool?,
       trId: json['trId'] as String?,
-      createdAt: json['createdAt'] == null
-          ? null
-          : DateTime.parse(json['createdAt'] as String),
+      createdAt: json['createdAt'] == null ? null : DateTime.parse(json['createdAt'] as String),
       isDefault: json['isDefault'] as bool?,
       isDeleted: json['isDeleted'] as bool?,
       isFinalState: json['isFinalState'] as bool?,
@@ -23,34 +21,8 @@ NextState _$NextStateFromJson(Map<String, dynamic> json) => NextState(
       canDelete: json['canDelete'] as bool?,
       tag: json['tag'] as List<dynamic>?,
       key: json['key'] as String?,
-      updatedAt: json['updatedAt'] == null
-          ? null
-          : DateTime.parse(json['updatedAt'] as String),
+      updatedAt: json['updatedAt'] == null ? null : DateTime.parse(json['updatedAt'] as String),
       id: json['id'] as int?,
-      label:
-          (json['label'] as List<dynamic>?)?.map((e) => e as String).toList(),
-      userGroups: (json['userGroups'] as List<dynamic>?)
-          ?.map((e) => WorkSpaceUserGroups.fromJson(e as Map<String, dynamic>))
-          .toList(),
+      label: (json['label'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      userGroups: (json['userGroups'] as List<dynamic>?)?.map((e) => WorkSpaceUserGroups.fromJson(e as Map<String, dynamic>)).toList(),
     );
-
-Map<String, dynamic> _$NextStateToJson(NextState instance) => <String, dynamic>{
-      'canDisplay': instance.canDisplay,
-      'description': instance.description,
-      'className': instance.className,
-      'isActive': instance.isActive,
-      'trId': instance.trId,
-      'createdAt': instance.createdAt?.toIso8601String(),
-      'isDefault': instance.isDefault,
-      'isDeleted': instance.isDeleted,
-      'isFinalState': instance.isFinalState,
-      'mainStateId': instance.mainStateId,
-      'name': instance.name,
-      'canDelete': instance.canDelete,
-      'tag': instance.tag,
-      'key': instance.key,
-      'updatedAt': instance.updatedAt?.toIso8601String(),
-      'id': instance.id,
-      'label': instance.label,
-      'userGroups': instance.userGroups,
-    };
--- lib/feature/models/work_space/generator/work_space_user_groups.g.dart
+++ lib/feature/models/work_space/generator/work_space_user_groups.g.dart
@@ -6,18 +6,8 @@ part of '../work_space_user_groups.dart';
 // JsonSerializableGenerator
 // **************************************************************************
 
-WorkSpaceUserGroups _$WorkSpaceUserGroupsFromJson(Map<String, dynamic> json) =>
-    WorkSpaceUserGroups(
+WorkSpaceUserGroups _$WorkSpaceUserGroupsFromJson(Map<String, dynamic> json) => WorkSpaceUserGroups(
       id: json['id'] as int?,
-      labels:
-          (json['labels'] as List<dynamic>?)?.map((e) => e as String).toList(),
+      labels: (json['labels'] as List<dynamic>?)?.map((e) => e as String).toList(),
       name: json['name'] as String?,
     );
-
-Map<String, dynamic> _$WorkSpaceUserGroupsToJson(
-        WorkSpaceUserGroups instance) =>
-    <String, dynamic>{
-      'id': instance.id,
-      'labels': instance.labels,
-      'name': instance.name,
-    };
