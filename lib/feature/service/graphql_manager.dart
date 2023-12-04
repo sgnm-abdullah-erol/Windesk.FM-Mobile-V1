7b537f2f3967af7e54c1b52a2d2641f2f662dd8c
MertSaygili <mert71719601@gmail.com>
2023-12-04 08:26:44 +0300

    common fix

--- lib/feature/service/graphql_manager.dart
+++ lib/feature/service/graphql_manager.dart
@@ -1,14 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
-import 'package:vm_fm_4/core/database/shared_manager.dart';
-import 'package:vm_fm_4/core/enums/shared_enums.dart';
 import 'package:vm_fm_4/feature/global_providers/global_provider.dart';
 
 class GraphQLManager {
   static final HttpLink _httpLink = HttpLink('http://localhost:3015/graphql');
-
-  static final String _userToken = SharedManager().getString(SharedEnum.userToken) as String;
 
   static ValueNotifier<GraphQLClient> _client = ValueNotifier<GraphQLClient>(
     GraphQLClient(
@@ -18,12 +14,7 @@ class GraphQLManager {
   );
 
   static ValueNotifier<GraphQLClient> getClient(HttpLink link) {
-
-  final HttpLink httpLink = HttpLink(
-    'http://localhost:3015/graphql',
-  );
-
-_client = ValueNotifier<GraphQLClient>(
+    _client = ValueNotifier<GraphQLClient>(
       GraphQLClient(
         link: link,
         cache: GraphQLCache(),
@@ -32,19 +23,13 @@ _client = ValueNotifier<GraphQLClient>(
     return _client;
   }
 
-static ValueNotifier<GraphQLClient> getClientForMutation(BuildContext context) {
+  static ValueNotifier<GraphQLClient> getClientForMutation(BuildContext context) {
+    final HttpLink httpLink = HttpLink('http://localhost:3015/graphql');
+    final AuthLink authLink = AuthLink(getToken: () async => 'Bearer ${context.read<GlobalProvider>().globalUserToken}');
 
-  final HttpLink httpLink = HttpLink(
-    'http://localhost:3015/graphql'
-  );
-  final AuthLink authLink = AuthLink(
-    getToken: () async => 'Bearer ${context.read<GlobalProvider>().globalUserToken}',
-    // OR
-    // getToken: () => 'Bearer <YOUR_PERSONAL_ACCESS_TOKEN>',
-  );
-  final Link link = authLink.concat(httpLink);
 
-  _client = ValueNotifier<GraphQLClient>(
+    final Link link = authLink.concat(httpLink);
+    _client = ValueNotifier<GraphQLClient>(
       GraphQLClient(
         link: link,
         cache: GraphQLCache(),
